09bd0fad44fe8d0a1782fdb76b6e5b7735788f2a
Yuan-Sama <nbnguyen9557@gmail.com>
2025-02-18 12:09:32 +0700

    init chart component

---
+++ src/components/chart/index.ts
@@ -0,0 +1,22 @@
+import Container from './chart-container.svelte';
+import Tooltip from './chart-tooltip.svelte';
+import TooltipContent from './chart-tooltip-content.svelte';
+import Legend from './chart-legend.svelte';
+import LegendContent from './chart-legend-content.svelte';
+import Style from './chart-style.svelte';
+
+export {
+	Container,
+	Tooltip,
+	TooltipContent,
+	Legend,
+	LegendContent,
+	Style,
+	//
+	Container as ChartContainer,
+	Tooltip as ChartTooltip,
+	TooltipContent as ChartTooltipContent,
+	Legend as ChartLegend,
+	LegendContent as ChartLegendContent,
+	Style as ChartStyle
+};
